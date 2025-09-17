531f70ae60c68bbdbdfe9043a4b95f07c5a640f4
mmarcondesm <matheus.marcondes@outlook.com>
2025-09-17 09:31:44 -0300

    New vision up

---
+++ app/Moviments/index.tsx
@@ -0,0 +1,60 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import React, { useState } from 'react';
+
+
+
+export default function Moviments({data}) {
+    const [showValue, setShowValue] = useState(false);
+    
+    return(
+        <TouchableOpacity style={styles.container}>
+                <Text style={styles.date}>{data.date}</Text>
+            
+            <View style={styles.content}>
+                <Text style={styles.label}>{data.label}</Text>
+
+                { showValue ? (
+                    <Text style={datatype === 1 ? styles.value : styles.expenses}>
+                        {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
+                        </Text>
+                        ) : (
+                            <View style={styles.skeleton}>
+                            </View>
+                        )}
+                    
+                
+
+                <Text style={styles.value}>
+                    {data.value}
+                </Text>
+            </View>
+
+        </TouchableOpacity>
+      
+    )
+}
+
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        marginBottom: 24,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#DADADA'
+    },
+    content:{
+        flexDirection: 'row'
+    },
+    label:{
+
+    },
+    date:{
+
+    },
+    value:{
+
+    }
+
+})
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,73 @@
-import { View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import Balance from "./Balance";
 import Header from "./Header";
+import Moviments from "./Moviments";
+
+const list = [
+  {
+    id: 1,
+    label: 'Conta de Luz',
+    Value: '300,00',
+    date: '17/09/2022',
+    type: 0
+  },
+  {
+    id: 2,
+    label: 'Pix Cliente X',
+    value: '2.000,00',
+    date: '10/09/2022',
+    type: 1
+  },
+  {
+    id: 3,
+    label: 'Pix Cliente Y',
+    value: '1.800,00',
+    date: '10/09/2022',
+    type: 1
+  },
+  {
+    id: 4,
+    label: 'Conta de Água',
+    Value: '300,00',
+    date: '17/09/2022',
+    type: 0
+  }
+]
 
 export default function Index() {
   return (
-    <View>
+    <View style={styles.container}>
       <Header name="Matheus Marcondes"/>
       
+      <Balance saldo="300.000,00" gasto="-15.000,00" />
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+
+      <FlatList style={styles.list}
+      data={list}
+      keyExtractor={(item) => String(item.id)}
+      showsVerticalScrollIndicator={false}
+      renderItem={ ({ item }) => <Moviments data={item}/> }
+      />
+
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#fafafa'
+  },
+  title:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14,
+  },
+  list:{
+    marginStart: 14,
+    marginEnd: 14
+  }
+})
